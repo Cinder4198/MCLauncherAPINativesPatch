a1eb574ed1e2a14c47f8e24c771c3536e366ac6e
Eyasu <67652147+Cinder4198@users.noreply.github.com>
2024-04-07 21:15:30 -0500

    Update NativesCommandPatcher.java
    Edited command patcher to look for the natives path argument instead of assuming its the 10th argument

--- NativesCommandPatcher.java
+++ NativesCommandPatcher.java
@@ -5,9 +5,21 @@ import java.util.List;
 
 public class NativesCommandPatcher {
     public List<String> patcher(List<String> command, String version) {
-         String newLibraryCmd = command.get(10).replaceAll("/natives", File.separator + version + "-natives");
-         List<String> newCommand = command;
-         newCommand.set(10, newLibraryCmd);
-         return newCommand;
+
+        for (int i = 0; i < command.size(); i++) {
+
+            String argument = command.get(i);
+
+            if(argument.contains("-Djava.library.path")) {
+
+                String newLibraryCmd = argument.replaceAll("/natives", File.separator + version + "-natives");
+                List<String> newCommand = command;
+                newCommand.set(i, newLibraryCmd);
+                System.out.println("New library arg: " + newLibraryCmd );
+                return newCommand;
+
+            }
+        }
+        return command;
     }
 }
